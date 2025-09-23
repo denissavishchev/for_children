4fc0882d30ed357c04a12135a96b57ece239917d
Denis Savishchev <denis.savishchev@gmail.com>
2025-09-23 17:24:43 +0200

    task selection1

--- lib/widgets/parents_widget/select_task_type_widget.dart
+++ lib/widgets/parents_widget/select_task_type_widget.dart
@@ -12,27 +12,62 @@ class SelectTaskTypeWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<ParentProvider>(
         builder: (context, data, _){
-          return Column(
-            children: [
-              Container(
+          return GestureDetector(
+            onTap: () => selectType(context, data),
+            child: Container(
+              width: 100,
+              height: 50,
+              decoration: BoxDecoration(
                 color: kDarkGrey,
-                width: 196,
-                child: Wrap(
-                  spacing: 12,
-                  runSpacing: 12,
-                  children: List.generate(data.taskTypes.length, (index){
-                    return Container(
-                      width: 40,
-                      height: 40,
-                      color: kRed,
-                    );
-                  }),
-                ),
+                borderRadius: const BorderRadius.all(Radius.circular(12)),
               ),
-              Text(data.selectedTypeStatus, style: kTextStyle,),
-            ],
+              child: Text('add'),
+            ),
           );
         }
     );
+  }
+
+  Future<void> selectType(context, ParentProvider data)async {
+    Size size = MediaQuery.sizeOf(context);
+    return showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        isDismissible: false,
+        backgroundColor: Colors.transparent,
+        builder: (context) {
+          return GestureDetector(
+            onTap: () {},
+            child: Container(
+                height: size.height * 0.2,
+                width: size.width,
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                margin: const EdgeInsets.only(bottom: 300),
+                decoration: const BoxDecoration(
+                  color: kGrey,
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+                child: Column(
+                  children: [
+                    Wrap(
+                      spacing: 12,
+                      runSpacing: 12,
+                      children: List.generate(data.taskTypes.length, (index){
+                        return Container(
+                          width: 40,
+                          height: 40,
+                          color: kRed,
+                        );
+                      }),
+                    ),
+                    Text(data.selectedTypeStatus, style: kTextStyle,),
+                    IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: Icon(Icons.close))
+                  ],
+                )
+            ),
+          );
+        });
   }
 }
