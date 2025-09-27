052d0ff73d3dc9c2e63e0501cea28e3d0144ddbf
Denis Savishchev <denis.savishchev@gmail.com>
2025-09-27 17:53:19 +0200

    task_square_widget

---
+++ lib/widgets/task_square_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class TaskSquareWidget extends StatelessWidget {
+  const TaskSquareWidget({
+    super.key,
+    required this.number,
+  });
+
+  final int number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 16,
+      height: 16,
+      decoration: BoxDecoration(
+        color: number == 0
+            ? kBlue.withValues(alpha: 0.1)
+            : number == 1
+            ? kBlue
+            : number == 2
+            ? kGreen.withValues(alpha: 0.8)
+            : kRed.withValues(alpha: 0.7),
+        borderRadius: BorderRadius.circular(4),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withValues(alpha: 0.1),
+              blurRadius: 1,
+              spreadRadius: 1,
+              offset: const Offset(1, 1))
+        ],
+        border: Border.all(
+            width: 2,
+            color: kWhite.withValues(alpha: 0.7)),
+      ),
+    );
+  }
+}
