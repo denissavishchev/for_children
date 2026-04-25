64b7a54a7113bbd92849582b5b0aa43f7ac816a1
Devis <denis.savishchev@gmail.com>
2026-04-25 11:15:23 +0200

    wish list screen new ui,

--- lib/widgets/kids_widgets/wishes_tiles_list_widget.dart
+++ lib/widgets/kids_widgets/wishes_tiles_list_widget.dart
@@ -1,4 +1,3 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -51,156 +50,166 @@ class _WishesTilesListWidgetState extends State<WishesTilesListWidget> {
                                 : null;
                           },
                           child: Container(
-                            height: 120,
                             margin: const EdgeInsets.symmetric(vertical: 3),
-                            padding: const EdgeInsets.only(right: 6, left: 12),
                             decoration: BoxDecoration(
-                                gradient: LinearGradient(
-                                    colors: [
-                                      kBlue.withValues(alpha: 0.2),
-                                      kWhite,
-                                    ]
+                              color: kWhite,
+                                gradient: RadialGradient(
+                                  colors: [
+                                    kWhite,
+                                    kPurple.withValues(alpha: 0.3),
+                                    kLightBlue.withValues(alpha: 0.3),
+                                  ],
+                                  center: Alignment.bottomRight,
+                                  radius: 1.7,
                                 ),
-                                border: Border.all(width: 1, color: kDarkWhite),
+                                border: Border.all(width: 1, color: kPurple),
                                 borderRadius: const BorderRadius.all(Radius.circular(8)),
                                 boxShadow: [
                                   BoxShadow(
-                                      color: kDarkGrey.withValues(alpha: 0.3),
+                                      color: kDarkBlue.withValues(alpha: 0.25),
                                       blurRadius: 4,
                                       spreadRadius: 2,
                                       offset: Offset(4, 4)
                                   )
                                 ]
                             ),
-                            child: Row(
-                              spacing: 12,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            child: Stack(
                               children: [
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 12),
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Text(wishes[index]['wish'],
-                                              style: kBigTextStyleWhite.copyWith(fontSize: 44.sp),),
-                                            Container(
-                                              padding: const EdgeInsets.fromLTRB(2, 2, 4, 2),
-                                              decoration: BoxDecoration(
-                                                gradient: LinearGradient(
-                                                    colors: [
-                                                      kOrange.withValues(alpha: 0.6),
-                                                      kOrange.withValues(alpha: 0.5),
-                                                      kOrange.withValues(alpha: 0.4),
-                                                    ]
-                                                ),
-                                                borderRadius: const BorderRadius.all(Radius.circular(18)),
-                                                border: Border.all(width: 0.5, color: kOrange.withValues(alpha: 0.5)),
-                                              ),
-                                              child: Row(
+                                Positioned(
+                                  top: -40,
+                                  left: -40,
+                                  child: Icon(
+                                      Icons.star,
+                                      color: kPurple.withValues(alpha: 0.1),
+                                      size: 300.sp)),
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(12, 4, 8, 4),
+                                  child: Row(
+                                    spacing: 12,
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(vertical: 8),
+                                          child: Column(
+                                            spacing: 8,
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Row(
+                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                                 children: [
-                                                  Icon(
-                                                    Icons.star,
-                                                    color: kOrange,
-                                                    size: 16,
-                                                    shadows: [
-                                                      Shadow(
-                                                        color: kWhite,
-                                                        blurRadius: 3,
-                                                        offset: const Offset(0.2, 0.2),
-                                                      )
-                                                    ],
+                                                  Text(wishes[index]['wish'],
+                                                    style: kBigTextStyle.copyWith(fontSize: 44.sp, height: 1.0),),
+                                                  Container(
+                                                    padding: const EdgeInsets.all(1),
+                                                    decoration: BoxDecoration(
+                                                      shape: BoxShape.circle,
+                                                      gradient: LinearGradient(
+                                                          colors: [
+                                                            kOrange.withValues(alpha: 0.4),
+                                                            kOrange.withValues(alpha: 0.5),
+                                                            kOrange.withValues(alpha: 0.6),
+                                                          ],
+                                                        begin: Alignment.topLeft,
+                                                        end: Alignment.bottomRight
+                                                      ),
+                                                      border: Border.all(width: 0.5, color: kOrange.withValues(alpha: 0.5)),
+                                                    ),
+                                                    child: Icon(
+                                                      Icons.star,
+                                                      color: kOrange,
+                                                      size: 18,
+                                                      shadows: [
+                                                        Shadow(
+                                                          color: kWhite,
+                                                          blurRadius: 3,
+                                                          offset: const Offset(0.2, 0.2),
+                                                        )
+                                                      ],
+                                                    ),
                                                   ),
-                                                  Text('dreamItem'.tr(),
-                                                    style: kTextStyleWhite.copyWith(fontSize: 18.sp),
-                                                    textAlign: TextAlign.center,),
                                                 ],
                                               ),
-                                            ),
-                                          ],
+                                              Text(wishes[index]['whyNeed'],
+                                                style: kTextStyle.copyWith(fontSize: 18.sp),),
+                                            ],
+                                          ),
                                         ),
-                                        const Spacer(),
-                                        Text(wishes[index]['whyNeed'],
-                                          style: kTextStyleWhite.copyWith(fontSize: 18.sp),),
-                                      ],
-                                    ),
+                                      ),
+                                         wishes[index]['imageUrl'] == 'false'
+                                          ? Container(
+                                        width: size.width * 0.2,
+                                        height: 100,
+                                        clipBehavior: Clip.hardEdge,
+                                        decoration: BoxDecoration(
+                                            gradient: LinearGradient(
+                                                colors: [
+                                                  kLightBlue.withValues(alpha: 0.2),
+                                                  kBlue,
+                                                ],
+                                                begin: Alignment.topLeft,
+                                                end: Alignment.bottomRight
+                                            ),
+                                            borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                            border: Border.all(width: 0.5, color: kWhite),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: kGrey,
+                                                blurRadius: 10,
+                                                spreadRadius: 1,
+                                                offset: const Offset(1, 1),
+                                              ),
+                                              BoxShadow(
+                                                color: kWhite.withValues(alpha: 0.5),
+                                                blurRadius: 10,
+                                                spreadRadius: 1,
+                                                offset: const Offset(-2, -2),
+                                              ),
+                                            ]
+                                        ),
+                                        child: Icon(Icons.no_photography, color: kDarkBlue, size: 84.sp,),
+                                      )
+                                          : Container(
+                                            width: size.width * 0.2,
+                                            height: 100,
+                                            clipBehavior: Clip.hardEdge,
+                                            decoration: BoxDecoration(
+                                              gradient: LinearGradient(
+                                                  colors: [
+                                                    kLightBlue.withValues(alpha: 0.2),
+                                                    kBlue,
+                                                  ],
+                                                  begin: Alignment.topLeft,
+                                                  end: Alignment.bottomRight
+                                              ),
+                                              borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                              boxShadow: [
+                                                BoxShadow(
+                                                  color: kLightBlue.withValues(alpha: 0.2),
+                                                  blurRadius: 10,
+                                                  spreadRadius: 1,
+                                                  offset: const Offset(1, 1),
+                                                ),
+                                                BoxShadow(
+                                                  color: kWhite.withValues(alpha: 0.5),
+                                                  blurRadius: 10,
+                                                  spreadRadius: 1,
+                                                  offset: const Offset(-2, -2),
+                                                ),
+                                              ]
+                                            ),
+                                            child: Image.network(wishes[index]['imageUrl'],
+                                              fit: BoxFit.cover,
+                                              errorBuilder: (context, error, stackTrace) {
+                                                return Center(
+                                                  child: Image.asset('assets/images/headphones.png'),
+                                                );
+                                              },),
+                                          )
+                                    ],
                                   ),
                                 ),
-                                   wishes[index]['imageUrl'] == 'false'
-                                    ? Container(
-                                  width: size.width * 0.2,
-                                  height: 100,
-                                  clipBehavior: Clip.hardEdge,
-                                  decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                          colors: [
-                                            kLightBlue.withValues(alpha: 0.2),
-                                            kBlue,
-                                          ],
-                                          begin: Alignment.topLeft,
-                                          end: Alignment.bottomRight
-                                      ),
-                                      borderRadius: const BorderRadius.all(Radius.circular(8)),
-                                      border: Border.all(width: 0.5, color: kWhite),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: kGrey,
-                                          blurRadius: 10,
-                                          spreadRadius: 1,
-                                          offset: const Offset(1, 1),
-                                        ),
-                                        BoxShadow(
-                                          color: kWhite.withValues(alpha: 0.5),
-                                          blurRadius: 10,
-                                          spreadRadius: 1,
-                                          offset: const Offset(-2, -2),
-                                        ),
-                                      ]
-                                  ),
-                                  child: Icon(Icons.no_photography, color: kDarkBlue, size: 84.sp,),
-                                )
-                                    : Container(
-                                      width: size.width * 0.2,
-                                      height: 100,
-                                      clipBehavior: Clip.hardEdge,
-                                      decoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                            colors: [
-                                              kLightBlue.withValues(alpha: 0.2),
-                                              kBlue,
-                                            ],
-                                            begin: Alignment.topLeft,
-                                            end: Alignment.bottomRight
-                                        ),
-                                        borderRadius: const BorderRadius.all(Radius.circular(8)),
-                                        border: Border.all(width: 0.5, color: kWhite),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: kGrey,
-                                            blurRadius: 10,
-                                            spreadRadius: 1,
-                                            offset: const Offset(1, 1),
-                                          ),
-                                          BoxShadow(
-                                            color: kWhite.withValues(alpha: 0.5),
-                                            blurRadius: 10,
-                                            spreadRadius: 1,
-                                            offset: const Offset(-2, -2),
-                                          ),
-                                        ]
-                                      ),
-                                      child: Image.network(wishes[index]['imageUrl'],
-                                        fit: BoxFit.cover,
-                                        errorBuilder: (context, error, stackTrace) {
-                                          return Center(
-                                            child: Image.asset('assets/images/headphones.png'),
-                                          );
-                                        },),
-                                    )
                               ],
                             ),
                           ),
