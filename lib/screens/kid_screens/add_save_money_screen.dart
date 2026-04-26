ced265c750060c19e5a1b42deef6041ea197dff8
Devis <denis.savishchev@gmail.com>
2026-04-26 12:15:16 +0200

    add new saving screen ui

--- lib/screens/kid_screens/add_save_money_screen.dart
+++ lib/screens/kid_screens/add_save_money_screen.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:for_children/screens/kid_screens/save_money_screen.dart';
 import 'package:provider/provider.dart';
 import '../../constants.dart';
 import '../../providers/kid_provider.dart';
-import '../../widgets/button_widget.dart';
+import '../../widgets/round_button.dart';
 
 class AddSaveMoneyScreen extends StatelessWidget {
   const AddSaveMoneyScreen({super.key});
@@ -25,107 +25,193 @@ class AddSaveMoneyScreen extends StatelessWidget {
               height: size.height,
               child: Stack(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-                          Row(
+                  SingleChildScrollView(
+                    child: Column(
+                      spacing: 12,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(12, 12, 12, 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              IconButton(
-                                  onPressed: () => Navigator.pushReplacement(context,
-                                      MaterialPageRoute(builder: (context) =>
-                                      const SaveMoneyScreen())),
-                                  icon: const Icon(
-                                    Icons.arrow_back_ios_new,
-                                    color: kGrey,
-                                    size: 32,
-                                  )),
+                              RoundButton(
+                                  onTap: () => Navigator.pushReplacement(context,
+                                      MaterialPageRoute(builder: (context) => const SaveMoneyScreen())),
+                                  icon: Icons.close
+                              ),
+                              Text('addYourNewSaving'.tr(), style: kBigTextStyle.copyWith(fontSize: 44.sp)),
+                              const SizedBox(width: 40,)
                             ],
                           ),
-                          GestureDetector(
-                            onTap: () => data.pickAnImage(),
-                            child: Container(
-                              padding: const EdgeInsets.all(8),
-                              width: size.width,
-                              height: size.height * 0.4,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                                  color: kGrey.withValues(alpha: 0.9)
-                              ),
-                              child: data.fileName == ''
-                                  ? const Icon(Icons.camera_alt, size: 80, color: kWhite,)
-                                  : ClipRRect(
-                                    clipBehavior: Clip.hardEdge,
-                                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                                      child: Image.file(File(data.file!.path), fit: BoxFit.cover,)),
-                            ),
-                          ),
-                          const SizedBox(height: 24,),
-                          Container(
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          child: Text('someAdvicesBelow'.tr(), style: kTextStyle),
+                        ),
+                        Container(
                             padding: const EdgeInsets.all(12),
                             decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(12)),
-                                color: kWhite.withValues(alpha: 0.8)
+                                borderRadius: BorderRadius.all(Radius.circular(18)),
+                                color: kWhite,
+                                border: Border.all(color: kDarkWhite, width: 2),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: kGrey.withValues(alpha: 0.5),
+                                      spreadRadius: 7,
+                                      blurRadius: 16,
+                                      offset: const Offset(0, 8)
+                                  )
+                                ]
                             ),
-                            child: Form(
-                              key: data.saveMoneyKey,
-                              child: Column(
-                                spacing: 8,
-                                children: [
-                                  TextFormField(
-                                    controller: data.whatDoYoWantController,
-                                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                                    cursorColor: kDarkGrey,
-                                    decoration: textFieldDecoration.copyWith(
-                                        label: Text('whatDoYouWant'.tr(),)),
-                                    maxLength: 64,
-                                    validator: (value){
-                                      if(value == null || value.isEmpty) {
-                                        return 'thisFieldCannotBeEmpty'.tr();
-                                      }
-                                      return null;
-                                    },
+                          child: Column(
+                            spacing: 12,
+                            children: [
+                              GestureDetector(
+                                onTap: () => data.pickAnImage(),
+                                child: Container(
+                                  width: size.width * 0.5,
+                                  height: size.width * 0.6,
+                                  clipBehavior: Clip.hardEdge,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: kDarkGrey, width: data.fileName == '' ? 2 : 0),
+                                    borderRadius: const BorderRadius.all(Radius.circular(18)),
                                   ),
-                                  TextFormField(
-                                    controller: data.whatDoYoWantPriceController,
-                                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                                    cursorColor: kDarkGrey,
-                                    decoration: textFieldDecoration.copyWith(
-                                        label: Text('price'.tr(),)),
-                                    maxLength: 64,
-                                    validator: (value){
-                                      if(value == null || value.isEmpty) {
-                                        return 'thisFieldCannotBeEmpty'.tr();
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                  TextFormField(
-                                    controller: data.whyYouNeedThisController,
-                                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                                    cursorColor: kDarkGrey,
-                                    decoration: textFieldDecoration.copyWith(
-                                        label: Text('whyYouNeedThis'.tr(),)),
-                                    maxLength: 64,
-                                    validator: (value){
-                                      if(value == null || value.isEmpty) {
-                                        return 'thisFieldCannotBeEmpty'.tr();
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                ],
+                                  child: data.fileName == ''
+                                      ? const Icon(Icons.camera_alt, size: 80, color: kDarkGrey,)
+                                      : ClipRRect(
+                                      clipBehavior: Clip.hardEdge,
+                                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                                      child: Image.file(File(data.file!.path), fit: BoxFit.cover,)),
+                                ),
                               ),
+                              Form(
+                                key: data.saveMoneyKey,
+                                child: Column(
+                                  spacing: 8,
+                                  children: [
+                                    TextFormField(
+                                      controller: data.whatDoYoWantController,
+                                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                                      cursorColor: kDarkGrey,
+                                      decoration: textFieldDecoration.copyWith(
+                                          label: Text('whatDoYouWant'.tr(),)),
+                                      maxLength: 64,
+                                      validator: (value){
+                                        if(value == null || value.isEmpty) {
+                                          return 'thisFieldCannotBeEmpty'.tr();
+                                        }
+                                        return null;
+                                      },
+                                    ),
+                                    TextFormField(
+                                      controller: data.whatDoYoWantPriceController,
+                                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                                      cursorColor: kDarkGrey,
+                                      decoration: textFieldDecoration.copyWith(
+                                          label: Text('price'.tr(),)),
+                                      maxLength: 64,
+                                      validator: (value){
+                                        if(value == null || value.isEmpty) {
+                                          return 'thisFieldCannotBeEmpty'.tr();
+                                        }
+                                        return null;
+                                      },
+                                    ),
+                                    TextFormField(
+                                      controller: data.whyYouNeedThisController,
+                                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                                      cursorColor: kDarkGrey,
+                                      decoration: textFieldDecoration.copyWith(
+                                          label: Text('whyYouNeedThis'.tr(),)),
+                                      maxLength: 128,
+                                      validator: (value){
+                                        if(value == null || value.isEmpty) {
+                                          return 'thisFieldCannotBeEmpty'.tr();
+                                        }
+                                        return null;
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            if(data.saveMoneyKey.currentState!.validate()){
+                              data.saveSaveMoneyItem(context);
+                            }
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            width: size.width * 0.7,
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: [
+                                    kOrange,
+                                    kRed,
+                                    kPurple,
+                                    kBlue,
+                                    kDarkBlue
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                  stops: [
+                                    0.1, 0.4, 0.6, 0.8, 0.9
+                                  ]
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: kOrange.withValues(alpha: 0.4),
+                                    blurRadius: 10,
+                                    spreadRadius: 1,
+                                    offset: const Offset(-4, 0)
+                                ),
+                                BoxShadow(
+                                    color: kDarkBlue.withValues(alpha: 0.25),
+                                    blurRadius: 7,
+                                    spreadRadius: 5,
+                                    offset: const Offset(0, 7)
+                                )
+                              ],
+                              border: Border.all(color: kDarkWhite.withValues(alpha: 0.8), width: 0.5),
+                              borderRadius: const BorderRadius.all(Radius.circular(18)),
                             ),
+                            child: Text('add'.tr(), style: kBigTextStyleWhite.copyWith(fontSize: 44.sp)),
                           ),
-                          ButtonWidget(
-                            onTap: () => data.saveSaveMoneyItem(context),
-                            text: 'add',
+                        ),
+                        const SizedBox(height: 12,),
+                        Container(
+                          margin: const EdgeInsets.fromLTRB(12, 0, 12, 12),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(18)),
+                            border: Border.all(color: kGrey, width: 1),
                           ),
-                          const SizedBox(height: 12,)
-                        ],
-                      ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('howToEarnAdviceTitle1'.tr(), style: kTextStyle),
+                              Text('howToEarnAdvice1'.tr(), style: kTextStyle),
+                              const SizedBox(height: 12,),
+                              Text('howToEarnAdviceTitle2'.tr(), style: kTextStyle),
+                              Text('howToEarnAdvice2'.tr(), style: kTextStyle),
+                              const SizedBox(height: 12,),
+                              Text('howToEarnAdviceTitle3'.tr(), style: kTextStyle),
+                              Text('howToEarnAdvice3'.tr(), style: kTextStyle),
+                              const SizedBox(height: 12,),
+                              Text('howToEarnAdviceTitle4'.tr(), style: kTextStyle),
+                              Text('howToEarnAdvice4'.tr(), style: kTextStyle),
+                              const SizedBox(height: 12,),
+                              Text('howToEarnAdviceTitle5'.tr(), style: kTextStyle),
+                              Text('howToEarnAdvice5'.tr(), style: kTextStyle),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   data.isLoading
