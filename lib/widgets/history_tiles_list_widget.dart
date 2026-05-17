53c43bf27bc667aac676abd8a0d0ef257eb6b16f
Devis <denis.savishchev@gmail.com>
2026-05-17 08:41:27 +0200

    history tile fix

--- lib/widgets/history_tiles_list_widget.dart
+++ lib/widgets/history_tiles_list_widget.dart
@@ -1,5 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:for_children/widgets/task_square_widget.dart';
 import 'package:provider/provider.dart';
 import '../constants.dart';
 import '../providers/parent_provider.dart';
@@ -11,7 +13,6 @@ class HistoryTilesListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.sizeOf(context);
     return Consumer<ParentProvider>(
         builder: (context, data, _){
           return ListView.builder(
@@ -19,6 +20,13 @@ class HistoryTilesListWidget extends StatelessWidget {
               itemBuilder: (context, index){
                 final history = snapshot[index];
                 if (history['parentEmail'].toLowerCase() == data.email){
+                  Size size = MediaQuery.sizeOf(context);
+                  List<int> counts = [0, 0, 0, 0];
+                  if(history['daysNumber'] != null){
+                    for (var n in history['daysNumber']) {
+                      counts[n] += 1;
+                    }
+                  }
                   return GestureDetector(
                     onTap: () => data.historyDescription(context,
                         history['price'],
@@ -27,8 +35,7 @@ class HistoryTilesListWidget extends StatelessWidget {
                     child: Container(
                         margin: const EdgeInsets.fromLTRB(12, 0, 12, 12),
                         width: size.width,
-                        height: 60,
-                        padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 8),
+                        padding: const EdgeInsets.all(8),
                         decoration: BoxDecoration(
                             color: kWhite,
                             border: Border.all(width: 1, color: kBlue.withValues(alpha: 0.2)),
@@ -47,93 +54,194 @@ class HistoryTilesListWidget extends StatelessWidget {
                               ),
                             ]
                         ),
-                        child: Row(
-                          spacing: 4,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        child: Column(
                           children: [
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              crossAxisAlignment: CrossAxisAlignment.start,
+                            Row(
+                              spacing: 4,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Text(data.role == 'parent'
-                                    ? history['kidName']
-                                    : history['parentName'], style: kTextStyle,),
-                                Text(history['taskName'], style: kTextStyle,),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      spacing: 4,
+                                      children: [
+                                        Image.asset('assets/images/person.png', width: 14,),
+                                        Text(history['kidName'], style: kBigTextStyle,),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                        width: 100,
+                                        child: const Divider(color: kBlue, thickness: 1,)),
+                                    Row(
+                                      spacing: 4,
+                                      children: [
+                                        Image.asset('assets/images/todo.png', width: 16,),
+                                        Text(history['taskName'], style: kBigTextStyle,),
+                                      ],
+                                    ),
+                                    Row(
+                                      spacing: 4,
+                                      children: [
+                                        SizedBox(width: 16,),
+                                        Text(history['description'], style: kTextStyle,),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  spacing: 2,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: List.generate(
+                                        history['daysNumber'] != null ? 5 : 3,
+                                            (i) => ((history['daysNumber'] != null ? 4 : 2) - i) < int.parse(history['expQty'])
+                                            ? SvgPicture.asset(
+                                          'assets/icons/pepper.svg',
+                                          width: 12,
+                                          colorFilter: const ColorFilter.mode(kRed, BlendMode.srcIn),
+                                        )
+                                            : const SizedBox.shrink(),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 85,
+                                      child: Stack(
+                                        children: [
+                                          Positioned(
+                                              top: 10,
+                                              left: 0,
+                                              child: Icon(
+                                                double.parse(history['stars']).toInt() >= 1
+                                                    ? Icons.star
+                                                    : Icons.star_border,
+                                                color: kWhite,
+                                                size: 40,
+                                                shadows: const [
+                                                  BoxShadow(
+                                                      color: kBlue,
+                                                      blurRadius: 9,
+                                                      spreadRadius: 6,
+                                                      offset: Offset(0.5, 0.5)
+                                                  )
+                                                ],
+                                              )),
+                                          Align(
+                                              alignment: Alignment.topCenter,
+                                              child: Icon(
+                                                double.parse(history['stars']).toInt() >= 2
+                                                    ? Icons.star
+                                                    : Icons.star_border,
+                                                color: kWhite,
+                                                size: 45,
+                                                shadows: const [
+                                                  BoxShadow(
+                                                      color: kBlue,
+                                                      blurRadius: 9,
+                                                      spreadRadius: 6,
+                                                      offset: Offset(0.5, 0.5)
+                                                  )
+                                                ],
+                                              )),
+                                          Positioned(
+                                              top: 10,
+                                              right: 0,
+                                              child: Icon(
+                                                double.parse(history['stars']).toInt() >= 3
+                                                    ? Icons.star
+                                                    : Icons.star_border,
+                                                color: kWhite,
+                                                size: 40,
+                                                shadows: const [
+                                                  BoxShadow(
+                                                      color: kBlue,
+                                                      blurRadius: 9,
+                                                      spreadRadius: 6,
+                                                      offset: Offset(0.5, 0.5)
+                                                  )
+                                                ],
+                                              )),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(height: 2,),
+                                    Row(
+                                      spacing: 4,
+                                      children: [
+                                        Image.asset('assets/images/medal.png', width: 14,),
+                                        Text(history['price'], style: kBigTextStyle,),
+                                      ],
+                                    )
+                                  ],
+                                )
                               ],
                             ),
-                            const Spacer(),
+                            history['daysNumber'] != null
+                              ? Container(
+                                  height: 65,
+                                  width: double.infinity,
+                                  margin: const EdgeInsets.only(top: 6),
+                                  padding: const EdgeInsets.only(left: 4, right: 8),
+                                  decoration: BoxDecoration(
+                                    color: kBlue.withValues(alpha: 0.1),
+                                    borderRadius: const BorderRadius.horizontal(right: Radius.circular(4)),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      SizedBox(
+                                        width: size.width * 0.46,
+                                        child: Wrap(
+                                          spacing: 4,
+                                          runSpacing: 4,
+                                          children: List.generate(
+                                            history['daysNumber'].length, (i) {
+                                            int number = history['daysNumber'][i];
+                                            return TaskSquareWidget(number: number);
+                                          },
+                                          ),
+                                        ),
+                                      ),
+                                      Row(
+                                        spacing: 4,
+                                        children: [
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  TaskSquareWidget(number: 0),
+                                                  Text('-', style: kTextStyle,),
+                                                  Text(counts[0].toString(), style: kTextStyle,),
+                                                ],
+                                              ),
+                                              Row(
+                                                children: [
+                                                  TaskSquareWidget(number: 1),
+                                                  Text('-', style: kTextStyle,),
+                                                  Text(counts[1].toString(), style: kTextStyle,),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              : SizedBox.shrink(),
+                            const SizedBox(height: 8,),
                             Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: List.generate(5, ((i){
-                                return SvgPicture.asset('assets/icons/pepper.svg',
-                                  width: 12,
-                                  colorFilter: ColorFilter.mode((4 - i) < int.parse(history['expQty'])
-                                      ? kRed : kWhite, BlendMode.srcIn),
-                                );
-                              })),
-                            ),
-                            Text(history['daysNumber'] != null
-                                ? 'M'
-                                : 'S', style: kTextStyle,),
-                            SizedBox(
-                              width: 85,
-                              child: Stack(
-                                children: [
-                                  Positioned(
-                                      top: 10,
-                                      left: 0,
-                                      child: Icon(
-                                        double.parse(history['stars']).toInt() >= 1
-                                            ? Icons.star
-                                            : Icons.star_border,
-                                        color: kWhite,
-                                        size: 40,
-                                        shadows: const [
-                                          BoxShadow(
-                                              color: kBlue,
-                                              blurRadius: 9,
-                                              spreadRadius: 6,
-                                              offset: Offset(0.5, 0.5)
-                                          )
-                                        ],
-                                      )),
-                                  Align(
-                                      alignment: Alignment.topCenter,
-                                      child: Icon(
-                                        double.parse(history['stars']).toInt() >= 2
-                                            ? Icons.star
-                                            : Icons.star_border,
-                                        color: kWhite,
-                                        size: 45,
-                                        shadows: const [
-                                          BoxShadow(
-                                              color: kBlue,
-                                              blurRadius: 9,
-                                              spreadRadius: 6,
-                                              offset: Offset(0.5, 0.5)
-                                          )
-                                        ],
-                                      )),
-                                  Positioned(
-                                      top: 10,
-                                      right: 0,
-                                      child: Icon(
-                                        double.parse(history['stars']).toInt() >= 3
-                                            ? Icons.star
-                                            : Icons.star_border,
-                                        color: kWhite,
-                                        size: 40,
-                                        shadows: const [
-                                          BoxShadow(
-                                              color: kBlue,
-                                              blurRadius: 9,
-                                              spreadRadius: 6,
-                                              offset: Offset(0.5, 0.5)
-                                          )
-                                        ],
-                                      )),
-                                ],
-                              ),
+                              spacing: 4,
+                              children: [
+                                Image.asset('assets/images/deadline.png', width: 12,),
+                                Text(DateFormat('dd-MM-yyyy HH:mm').format(DateTime.parse(history['time'])),
+                                  style: kTextStyle,),
+                              ],
                             ),
                           ],
                         )
